ea81f4aa115cd54a935b8422c28bf0fa7a5d79b9
Siva Nomula <Siva.Nomula@Preqin.com>
2022-02-11 21:25:39 +0530

    final changes

--- src/components/ViewTutorial.tsx
+++ src/components/ViewTutorial.tsx
@@ -1,41 +1,39 @@
-import { useState, useEffect } from "react"; 
-import {  useParams } from "react-router-dom";
-import ITutorialData from "../types/Tutorial"; 
-import TutorialDataService from "../services/TutorialService"; 
-import Map from '../components/Map/Map';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import ITutorialData from "../types/Tutorial";
+import TutorialDataService from "../services/TutorialService";
+import MapApi from "../components/Map/MapApi";
 
-function ViewTutorial ()  {
-  const props = useParams(); 
+function ViewTutorial() {
+  const props = useParams();
   const [currentTutorial, setCurrentTutorial] = useState<ITutorialData>({});
- 
 
   const getAddress = (p: ITutorialData) => {
     return p.street || "" + p.city || "" + p.state || "" + p.postal_code || "";
   };
-  
+
   const getLocation = (p: ITutorialData) => {
     // return  {
     //     address:  '1600 Amphitheatre Parkway, Mountain View, california.',
     //     lat: 37.42216,
     //     lng:-122.08427,
     //   };
-    return  {
-        address: p.street,
-        lat:p.latitude,
-        lng:p.longitude,
-      };
+    return {
+      address: p.street,
+      lat: p.latitude,
+      lng: p.longitude,
+    };
   };
 
-   
   useEffect(() => {
     TutorialDataService.get(props.id)
-    .then((response: any) => {
-      setCurrentTutorial(response.data);
-      console.log(response.data);
-    })
-    .catch((e: Error) => {
-      console.log(e);
-    });
+      .then((response: any) => {
+        setCurrentTutorial(response.data);
+        console.log(response.data);
+      })
+      .catch((e: Error) => {
+        console.log(e);
+      });
   }, [props.id]);
 
   return (
@@ -64,9 +62,16 @@ function ViewTutorial ()  {
                 </label>
                 {getAddress(currentTutorial)}
               </div>
-              <div><label><strong>city:</strong></label>{currentTutorial.city}</div>
+              <div>
+                <label>
+                  <strong>city:</strong>
+                </label>
+                {currentTutorial.city}
+              </div>
             </div>
-            <Map location={getLocation(currentTutorial)} zoomLevel={17} />
+            {currentTutorial.latitude  && (
+              <MapApi location={getLocation(currentTutorial)} />
+            )}
           </>
         )}
       </div>
